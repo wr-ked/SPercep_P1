a7ef83e77cc65e167191623e40e9c67f60b17386
wr-ked <hugo.villasan.gt@gmail.com>
2026-03-16 18:12:56 +0100

    Enhance chessboard calibration function with image processing and corner detection

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import glob
 import os
 
 
+def calibration_image_captures():       # TODO: Implementar esta función para capturar imágenes de calibración en vivo si no se encuentran imágenes en la carpeta.
+    return None
+
 def chessboard_calibration():
 
     # Dimensiones del patrón de ajedrez
@@ -14,7 +17,7 @@ def chessboard_calibration():
     columnas = 6
 
     # Criterio de terminación
-    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_INTER, 30, 0.001)  # 
+    termcriteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)  # 
     
     # Preparar puntos de objeto.
     objp = np.zeros((filas*columnas, 3), np.float32)
@@ -38,16 +41,38 @@ def chessboard_calibration():
     
     for fname in images:
         img = cv.imread(fname)
-        if img is None
+        if img is None:
             print(f"Error leyendo {fname}")
             continue
     
-    # Escala de grises para la detección de esquinas
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        # Escala de grises para la detección de esquinas
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    # Buscar esquinas con las dimensiones correctas (9x6)
-    ret, corners = cv.findChessboardCorners(gray, (filas, columnas), None
-    
+        # Buscar esquinas con las dimensiones correctas (9x6)
+        ret, corners = cv.findChessboardCorners(gray, (filas, columnas), None)
+        
+        if ret == True:
+            # Si encuentra el patrón, lo añadimos a la lista de puntos de objeto
+            print(f"Patrón encontrado en {fname}")
+            objpoints.append(objp)
+
+            # Refinamos las esquinas para mayor precisión y las añadimos a la lista de puntos de imagen
+            corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), termcriteria)
+            imgpoints.append(corners2)
+
+            # Dibujar esquinas detectadas en la imagen
+            cv.drawChessboardCorners(img, (filas, columnas), corners2, ret)
+            cv.imshow('Calibración', img)
+            cv.waitKey(500)  # Esperar medio segundo para mostrar la imagen
+        else:
+            print(f"No se detectó el patrón 9x6 en {fname}")
+
+    cv.destroyAllWindows()
+
+    # CALIBRACION DE LA CAMARA
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    return mtx, dist
+
 def aruco_calibration():
     return None
 
@@ -56,11 +81,6 @@ def charuco_calibration():
 
 def live_projection():
     return None
-
-def calibration_image_captures():       # TODO: Implementar esta función para capturar imágenes de calibración en vivo si no se encuentran imágenes en la carpeta.
-    return None
-
-
 
 def main():
     print("=== PRÁCTICA 1: CALIBRACIÓN Y REALIDAD AUMENTADA ===")
